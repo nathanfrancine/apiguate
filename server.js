8f6bdd9296359c990868f77a4771a0a6b0ebce7e
Nathan Francine Soto <nsoto@tigo.com.gt>
2017-06-06 16:27:28 -0600

    adding

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 //Created by: Ing. Nathan Francine Soto
 const express = require('express')
-const bodyParser = require('body-parser')
-const timeout = require('connect-timeout')
-const config = require('config');
 var serviceCustomer = require('./services/customer.js');
-var utils = require('./utils.js');
-
-const port = process.env.PORT || config.get('localServer.port')
-const app = express()
-
-app.use(timeout(config.get('localServer.timeout')))
-
-const router = express.Router()
+var app = express()
+var router = express.Router()
 
 //services
-router.get(config.get('localServer.servicePathCostumer'), serviceCustomer.customer);
+app.get('/', function (req, res) {
+  res.send('api Guatemala')
+})
 
-app.use('/', router);
-app.get('*', utils.error404)
-app.use(utils.errorHandler);
-app.listen(config.get('localServer.port'), config.get('localServer.host'))
+app.get('/hello',serviceCustomer.customer)
+app.listen(80)
--- services/customer.js
+++ services/customer.js
@@ -1,15 +1,5 @@
 //Created by: Ing. Nathan Francine Soto
-const dse = require('dse-driver');
-const Log = require('log');
-const fs = require('fs');
-const config = require('config');
-const moment = require('moment');
 
-var log = new Log('info', fs.createWriteStream(config.get('logs.log')));
-var SubscriberInfo = require('../models/SubscriberInfo');
-var utils = require('../utils.js');
-
-
-exports.customer = function(req, res, next) {
+exports.customer = function(req, res) {
     res.send('<h1> hello api Guatemala</h1>')
 }
